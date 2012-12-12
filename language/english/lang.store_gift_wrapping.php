1006ca13a00cc4adf63ca15dbcb4cf0fbe51c148
Michael Rog <michael@michaelrog.com>
2012-12-12 02:00:15 -0600

    pkg.io boilerplate

---
+++ language/english/lang.store_gift_wrapping.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+// Start inserting custom language keys/values here
+	
+);
+
+/* End of file lang.store_gift_wrapping.php */
+/* Location: /system/expressionengine/third_party/store_gift_wrapping/language/english/lang.store_gift_wrapping.php */
